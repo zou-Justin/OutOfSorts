ae52c586ec01d585cd088bbef6089ec761573cef
Justin Dylan <justin82049@gmail.com>
2020-12-15 13:14:34 -0500

    bubble sort attempt

--- Sorts.java
+++ Sorts.java
@@ -3,5 +3,16 @@ public class Sorts{
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+  public static void bubbleSort(int[] data){
+    for (int i = 0; i < data.length - 1;i++){
+      for (int j = 0; j < data.length - 1;j++){
+        if (data[i] > data[i+1]){
+          int a = data[i+1];
+          data[i+1] = data[i];
+          data[i] = data[i+1];
+        }
+      }
+    }
+
+  }
 }
