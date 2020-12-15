9a459e6ae6eec0b7764825b36b315b1ecdd5bc2b
Justin Dylan <justin82049@gmail.com>
2020-12-15 13:18:30 -0500

    switched j and i

--- Sorts.java
+++ Sorts.java
@@ -4,12 +4,12 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    for (int i = 0; i < data.length - 1;i++){
-      for (int j = 0; j < data.length - 1;j++){
-        if (data[i] > data[i+1]){
-          int a = data[i+1];
-          data[i+1] = data[i];
-          data[i] = a;
+    for (int i = 0; i < data.length;i++){
+      for (int j = 0; j < data.length-1;j++){
+        if (data[j] > data[j+1]){
+          int a = data[j+1];
+          data[j+1] = data[j];
+          data[j] = a;
         }
       }
     }
