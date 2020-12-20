d62ca81d4c7fccaf486e79da8015f2087c02616e
Justin Dylan <justin82049@gmail.com>
2020-12-19 21:58:18 -0500

    first attempt at insertion sort

--- Sorts.java
+++ Sorts.java
@@ -29,7 +29,13 @@ public class Sorts{
     }
   }
   public static void insertionSort(int[] data){
+    for (int i = 0;i < data.length-1;i++){
+      for (int j = 0; j < data.length-i;j++){
+        if (data[j] < data[i]){
+          data[i] = data[i+1];
+        }
+      }
+    }
 
-    
   }
 }
