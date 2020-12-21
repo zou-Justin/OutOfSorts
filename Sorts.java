ae15790eb743d4395f4f0dee4d597cbe6e31eef0
Justin Dylan <justin82049@gmail.com>
2020-12-21 13:21:56 -0500

    saw that runtime was too long and fixed bubble

--- Sorts.java
+++ Sorts.java
@@ -5,7 +5,7 @@ public class Sorts{
   */
   public static void bubbleSort(int[] data){
     for (int i = 0; i < data.length;i++){
-      for (int j = 0; j < data.length-1;j++){
+      for (int j = 0; j < data.length-i-1;j++){
         if (data[j] > data[j+1]){
           int a = data[j+1];
           data[j+1] = data[j];
