bb737bb7b11e4a7215468f051d207ed44819609b
Justin Dylan <justin82049@gmail.com>
2020-12-22 20:00:36 -0500

    added boolean to end bubble early

--- Sorts.java
+++ Sorts.java
@@ -1,16 +1,18 @@
 public class Sorts{
-  /**Bubble sort of an int array.
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
   public static void bubbleSort(int[] data){
-    for (int i = 0; i < data.length;i++){
+    int a = 0;
+    boolean swap = false;
+    for (int i = 0; i < data.length-1;i++){
       for (int j = 0; j < data.length-i-1;j++){
         if (data[j] > data[j+1]){
-          int a = data[j+1];
-          data[j+1] = data[j];
-          data[j] = a;
+          a = data[j];
+          data[j] = data[j+1];
+          data[j+1] = a;
+          swap = true;
         }
+      }
+      if (swap = false){
+        i = data.length-2;
       }
     }
   }
